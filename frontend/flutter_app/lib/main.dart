f94d23b900477c07684451ae8849ce30d233ef07
Acsard <temraire117@gmail.com>
2026-06-01 22:25:03 +0900

    카카오맵 표시 방식을 웹뷰에서 Javascript SDK로 변경

--- frontend/flutter_app/lib/main.dart
+++ frontend/flutter_app/lib/main.dart
@@ -26,11 +26,58 @@ class KakaoMapScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Test Kakao Map"),
       ),
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(
-          url: WebUri("https://map.kakao.com"),
-        ),
-      ),
+        body: InAppWebView(
+          initialData: InAppWebViewInitialData(
+            data: '''
+                  <!DOCTYPE html>
+                  <html>
+                  <head>
+                  <meta charset="utf-8">
+                  <meta name="viewport"
+                        content="width=device-width, initial-scale=1.0">
+                  
+                  <script src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=6ff5862f3b23c7383d8586db1f0f93c1&autoload=false"></script>
+                  
+                  <style>
+                  html, body {
+                    margin: 0;
+                    padding: 0;
+                    width: 100%;
+                    height: 100%;
+                  }
+                  
+                  #map {
+                    width: 100%;
+                    height: 100%;
+                  }
+                  </style>
+                  </head>
+                  
+                  <body>
+                  
+                  <div id="map"></div>
+                  
+                  <script>
+                  kakao.maps.load(function(){
+
+                      var container = document.getElementById('map');
+
+                      var options = {
+                          center: new kakao.maps.LatLng(36.6358, 127.4914),
+                          level: 3
+                      };
+
+                      var map = new kakao.maps.Map(container, options);
+
+                  });
+                  </script>
+                                    
+                  </body>
+                  </html>
+                  ''',
+          ),
+
+        )
     );
   }
 }
